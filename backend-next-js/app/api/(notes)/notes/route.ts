023bb3531a7a56931846b44b12e5fc01cab1b941
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-02-03 08:29:46 +0530

    Notes Req

---
+++ backend-next-js/app/api/(notes)/notes/route.ts
@@ -0,0 +1,19 @@
+export const GET = (req: Request) => {
+  try {
+  } catch (error) {}
+};
+
+export const POST = (req: Request) => {
+  try {
+  } catch (error) {}
+};
+
+export const PATCH = (req: Request) => {
+  try {
+  } catch (error) {}
+};
+
+export const DELETE = (req: Request) => {
+  try {
+  } catch (error) {}
+};
