13965640969a195fac344b7d4318d5ff38517ffa
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-02-09 09:08:39 +0530

    Basic Form

--- nextjs-validation/src/app/layout.tsx
+++ nextjs-validation/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <section className="mx-auto w-screen h-screen my-auto flex items-center justify-center">
+          {children}
+        </section>
       </body>
     </html>
   );
