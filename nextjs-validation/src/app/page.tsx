52920d77ff8b39756e94fdbc88e6f6403c8858d4
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-02-09 18:24:22 +0530

    Error Indiaction

--- nextjs-validation/src/app/page.tsx
+++ nextjs-validation/src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { userValidation } from "@/utils/UserValidation";
+import { useState } from "react";
 
 export default function Home() {
+  const [errors, setErrors] = useState<any>({
+    firstName: [""],
+    lastName: [""],
+    email: [""],
+    phone: [""],
+    address: [""],
+  });
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -11,13 +19,14 @@ export default function Home() {
     const validation = userValidation.safeParse(data);
     if (!validation.success) {
       console.log(validation.error.flatten().fieldErrors);
+      setErrors(validation.error?.flatten().fieldErrors);
       return;
     }
   };
   return (
     <div className=" p-10 rounded-xl bg-teal-400 min-w-[500px]">
       <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-        <div className="grid grid-cols-[100px,auto] gap-4 items-center">
+        <div className=" relative grid grid-cols-[100px,auto] gap-4 items-center">
           <label htmlFor="firstName" className="font-medium">
             First Name :
           </label>
@@ -27,8 +36,11 @@ export default function Home() {
             id="firstName"
             className="p-1.5 rounded bg-gray-200 text-gray-700 outline-none focus:ring-teal-600 ring-2"
           />
+          <p className="absolute top-[105%] right-2 text-xs text-orange-600">
+            {errors?.firstName?.length ? errors.firstName[0] : ""}
+          </p>
         </div>
-        <div className="grid grid-cols-[100px,auto] gap-4 items-center">
+        <div className="relative grid grid-cols-[100px,auto] gap-4 items-center">
           <label htmlFor="firstName" className="font-medium">
             Last Name :
           </label>
@@ -38,8 +50,11 @@ export default function Home() {
             id="lastName"
             className="p-1.5 rounded bg-gray-200 text-gray-700 outline-none focus:ring-teal-600 ring-2"
           />
+          <p className="absolute top-[105%] right-2 text-xs text-orange-600">
+            {errors?.lastName?.length ? errors.lastName[0] : ""}
+          </p>
         </div>
-        <div className="grid grid-cols-[100px,auto] gap-4 items-center">
+        <div className="relative grid grid-cols-[100px,auto] gap-4 items-center">
           <label htmlFor="email" className="font-medium">
             Email :
           </label>
@@ -49,8 +64,11 @@ export default function Home() {
             id="email"
             className="p-1.5 rounded bg-gray-200 text-gray-700 outline-none focus:ring-teal-600 ring-2"
           />
+          <p className="absolute top-[105%] right-2 text-xs text-orange-600">
+            {errors?.email?.length ? errors.email[0] : ""}
+          </p>
         </div>
-        <div className="grid grid-cols-[100px,auto] gap-4 items-center">
+        <div className="relative grid grid-cols-[100px,auto] gap-4 items-center">
           <label htmlFor="phone" className="font-medium">
             Phone :
           </label>
@@ -60,8 +78,11 @@ export default function Home() {
             id="phone"
             className="p-1.5 rounded bg-gray-200 text-gray-700 outline-none focus:ring-teal-600 ring-2"
           />
+          <p className="absolute top-[105%] right-2 text-xs text-orange-600">
+            {errors?.phone?.length ? errors.phone[0] : ""}
+          </p>
         </div>
-        <div className="grid grid-cols-[100px,auto] gap-4 items-center">
+        <div className=" relative grid grid-cols-[100px,auto] gap-4 items-center">
           <label htmlFor="address" className="font-medium">
             Address :
           </label>
@@ -70,6 +91,9 @@ export default function Home() {
             id="address"
             className="p-1.5 rounded bg-gray-200 text-gray-700 outline-none focus:ring-teal-600 ring-2"
           />
+          <p className="absolute top-[105%] right-2 text-xs text-orange-600">
+            {errors?.address?.length ? errors.address[0] : ""}
+          </p>
         </div>
         <div className="w-full flex items-center justify-center">
           <button
