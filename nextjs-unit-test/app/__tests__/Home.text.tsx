6e920ba04a0e5d1bd5c198a663d89094c31745b8
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-02-10 19:51:04 +0530

    More Examples

--- nextjs-unit-test/app/__tests__/Home.text.tsx
+++ nextjs-unit-test/app/__tests__/Home.text.tsx
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 
 import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "../page";
 
 function sum(a: number, b: number) {
   return a + b;
@@ -10,6 +11,18 @@ test("Adding 2+ 3 equals 5", () => {
   expect(sum(2, 3)).toBe(5);
   expect(sum(3, 6)).not.toBe(5);
 });
+
+//  Before and After Operations
+
+// beforeAll(() => { // This will run before all the test cases
+// AfterAll(() => { // This will run after all the test cases
+//   console.log("Before All");
+// });
+
+// beforeEach(() => { // This will run before each test cases
+// afterEach(() => { // This will run after each test cases
+//   console.log("Before Each");
+// });
 
 test(" Object Checking", () => {
   const data = { one: 2 };
@@ -44,5 +57,20 @@ describe("This is the Combination of Multiple Unit Tests that it async", () => {
   test("Testing the async function testing", async () => {
     const resp = await sampleFun();
     expect(resp).not.toEqual("frbhrfrf");
+  });
+});
+
+describe("The UI Cgecking", () => {
+  beforeEach(() => {
+    render(<Home />);
+  }); // So that we can render the component before each test cases
+  test("the Home Checkking", () => {
+    // render(<Home />);
+    expect(screen.getByText(/Loser/)).toBeInTheDocument();
+  });
+  test("Checking by Text Id", () => {
+    // render(<Home />);
+    const element = screen.getByTestId("homeId");
+    expect(element.textContent).toMatch(/is/);
   });
 });
--- nextjs-unit-test/app/page.tsx
+++ nextjs-unit-test/app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid  grid-cols-1 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      data-testid="homeId"
+      className="grid  grid-cols-1 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+    >
+      Hari is a Loser
     </div>
   );
 }
