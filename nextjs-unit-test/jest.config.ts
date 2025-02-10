ba037402cfa3fb77b1d1aa637c5ca73e285a7879
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-02-10 13:19:13 +0530

    Jest Config

--- nextjs-unit-test/jest.config.ts
+++ nextjs-unit-test/jest.config.ts
@@ -3,7 +3,13 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -137,8 +143,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
-
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
 
@@ -196,4 +201,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
